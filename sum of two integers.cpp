f9c47b212a5295cce6f1c0f8f6bf3dc50f82bc05
shrutixid <123883088+shrutixid@users.noreply.github.com>
2023-02-04 10:22:05 +0530

    Add files via upload
    Program to find sum of two integers

---
+++ sum of two integers.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;// variables of name 'a','b','c' has been declared of  integer data type
+	a=10;// the variable 'a' has been initialised with a value
+	b=20;// the variable 'b' has been initialised with a value
+	c=a+b;// the sum of the two inetgers will be stored in the variable 'c'
+	printf("The sum of %d and %d is %d",a,b,c);
+}
